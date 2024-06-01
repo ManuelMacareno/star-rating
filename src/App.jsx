c17c8bd7ecf9266cae85e0a620446e451321153a
manu.macareno <manu.macareno@gmail.com>
2024-06-01 13:47:33 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import StarRating from "./components/index"
+import "./App.css"
+
+function App() {
+
+  return (
+    <div className="App">
+      <StarRating noOfStars={10} />
+    </div>
+  )
+}
+
+export default App
